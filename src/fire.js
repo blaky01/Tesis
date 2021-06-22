1fe018c64cd5192704486822f6844a956a4e6591
Franklin Liriano <franklin___elmejor@hotmail.com>
2021-06-22 17:06:35 -0400

    firstcommit

---
+++ src/fire.js
@@ -0,0 +1,15 @@
+import firebase from "firebase";
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAPdtb-_Lqaqh_lLQb088M9YxWTi808Rk4",
+    authDomain: "tesis-login-8fe2a.firebaseapp.com",
+    projectId: "tesis-login-8fe2a",
+    storageBucket: "tesis-login-8fe2a.appspot.com",
+    messagingSenderId: "751702807812",
+    appId: "1:751702807812:web:380e8f23cee791fc36ac51"
+  };
+  // Initialize Firebase
+const fire = firebase.initializeApp(firebaseConfig);
+
+export default fire;
